99f99f40287ae6126b9805df3f7f38126560e8ef
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-08 19:01:31 -0300

    feat(Logo): use Gradient

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,6 +1,8 @@
 const React = require("react")
 const Ink = require("ink")
 
+const Gradient = require("ink-gradient")
+
 // made with figlet, using "Elite" font
 // https://github.com/xero/figlet-fonts/blob/master/Elite.flf
 // https://web.archive.org/web/20120819044459/http://www.roysac.com/thedrawfonts-tdf.asp
@@ -14,6 +16,10 @@ const logo = `
 
 module.exports = class Logo extends React.Component {
     render() {
-        return <Ink.Text>{logo}</Ink.Text>
+        return (
+            <Gradient name="pastel">
+                <Ink.Text>{logo}</Ink.Text>
+            </Gradient>
+        )
     }
 }
