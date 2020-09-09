a6b235dd50093f4263d2bb890682c9ca8b1ce768
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-09 03:22:46 -0300

    feat(Logo): include xroia version

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,18 +1,22 @@
+const xroia = require("../../package.json")
+
 const React = require("react")
 const Ink = require("ink")
 
 const Gradient = require("ink-gradient")
 
+// gradient is rendered differently for last line otherwise
+const s = " ".repeat(xroia.version.length)
 // made with figlet, using "Elite" font
 // https://github.com/xero/figlet-fonts/blob/master/Elite.flf
 // https://web.archive.org/web/20120819044459/http://www.roysac.com/thedrawfonts-tdf.asp
 const logo = `
-▐▄• ▄ ▄▄▄        ▪   ▄▄▄·
- █▌█▌▪▀▄ █·▪     ██ ▐█ ▀█
- ·██· ▐▀▀▄  ▄█▀▄ ▐█·▄█▀▀█
-▪▐█·█▌▐█•█▌▐█▌.▐▌▐█▌▐█ ▪▐▌
-•▀▀ ▀▀.▀  ▀ ▀█▄▀▪▀▀▀ ▀  ▀
-`
+▐▄• ▄ ▄▄▄         ▪   ▄▄▄· ${s}
+ █▌█▌▪▀▄ █·▪     ██ ▐█ ▀█  ${s}
+ ·██· ▐▀▀▄  ▄█▀▄ ▐█·▄█▀▀█  ${s}
+▪▐█·█▌▐█•█▌▐█▌.▐▌▐█▌▐█ ▪▐▌ ${s}
+•▀▀ ▀▀.▀  ▀ ▀█▄▀▪▀▀▀ ▀  ▀ v${xroia.version}
+`.slice(1) // remove first newline
 
 module.exports = class Logo extends React.Component {
     render() {
