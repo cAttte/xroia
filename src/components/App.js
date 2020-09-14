f0cfda59ba5c9763511056bd1889e18e549e4f61
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-14 13:26:13 -0300

    refactor: rename app.js to App.js

---
+++ src/components/App.js
@@ -0,0 +1,66 @@
+const keypress = require("keypress")
+const importJSX = require("import-jsx")
+const React = require("react")
+
+const checkUpdates = require("../checkUpdates")
+const ColorPicker = importJSX("./ColorPicker")
+const SizeWarning = importJSX("./SizeWarning")
+const AutoUpdater = importJSX("./AutoUpdater")
+const Logo = importJSX("./ColorPicker/Logo")
+
+keypress(process.stdin)
+process.stdin.setRawMode(true)
+process.stdin.resume()
+
+module.exports = class App extends React.Component {
+    constructor(props) {
+        super(props)
+        const isMinSize = (process.stdout.columns / 100) * 50 >= Logo.getWidth() + 8
+        const window = isMinSize ? "color-picker" : "size-warning"
+        this.state = { window, detail: "Checking for updates..." }
+    }
+
+    componentDidMount() {
+        process.stdout.on("resize", this.handleResize.bind(this))
+        process.stdin.on("keypress", this.handleKeypress.bind(this))
+        this.updateCheck = checkUpdates().then(this.handleUpdateCheck.bind(this))
+    }
+
+    componentWillUnmount() {
+        process.stdout.off("resize", this.handleResize.bind(this))
+        process.stdin.off("keypress", this.handleKeypress.bind(this))
+        this.updateCheck.cancel()
+    }
+
+    render() {
+        const props = {
+            detail: this.state.detail,
+            update: this.state.update,
+            setWindow: this.setWindow.bind(this)
+        }
+
+        if (this.state.window === "color-picker") return <ColorPicker {...props} />
+        else if (this.state.window === "auto-updater") return <AutoUpdater {...props} />
+        else if (this.state.window === "size-warning") return <SizeWarning />
+    }
+
+    setWindow(window) {
+        this.setState({ window })
+    }
+
+    handleResize() {
+        if (this.state.window !== "color-picker") return
+        const isMinSize = (process.stdout.columns / 100) * 50 >= Logo.getWidth() + 8
+        if (!isMinSize) this.setState({ window: "size-warning" })
+        else this.setState({ window: "color-picker" })
+    }
+
+    handleKeypress(_, key) {
+        if (key && key.ctrl && key.name === "c") process.stdin.pause()
+    }
+
+    handleUpdateCheck(update) {
+        if (update.latest) this.setState({ detail: "v" + update.cur })
+        else this.setState({ update: update.new, window: "auto-updater" })
+    }
+}
