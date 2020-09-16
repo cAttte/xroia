bdab58943786b7febccd20effbf51104078b92da
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-15 21:17:20 -0300

    feat: hide cursor

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const Ink = require("ink")
 const importJSX = require("import-jsx")
 const App = importJSX("./components/App")
 const keypress = require("keypress")
+const ansi = require("ansi-escapes")
 
 keypress(process.stdin)
 process.stdin.setRawMode(true)
@@ -14,4 +15,5 @@ process.stdin.on("keypress", (_, key) => {
     if (key && key.ctrl && key.name === "c") process.exit(0)
 })
 
+process.stdout.write(ansi.cursorHide)
 Ink.render(React.createElement(App))
